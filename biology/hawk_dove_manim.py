1e3d6c5a12ee14dcfdf6922bc8a3df5a8f9c39f4
kejtos <47796150@fsv.cuni.cz>
2023-08-03 21:01:11 +0200

    Finish of the main table

--- biology/hawk_dove_manim.py
+++ biology/hawk_dove_manim.py
@@ -2,7 +2,6 @@ from manim import *
 from manim.opengl import *
 import numpy as np
 
-
 X_MOVE = 5.5
 Y_MOVE = 2
 PAUSE = 0.8
@@ -17,7 +16,7 @@ CONS_TOP = 45
 CONS_BOT = 45
 CONS_MID = 10
 TIMES = (N_TOP/DEN_TOP, CONS_TOP, CONS_BOT, N_BOT/DEN_BOT, CONS_MID)
-DECS = (2,0,0,2,0)
+DECS = (2, 0, 0, 2, 0)
 DEF_COL = BLUE_D
 TABLE_WIDTH = 10.0
 TABLE_HEIGHT = 5.0
@@ -25,10 +24,10 @@ TABLE_HEIGHT = 5.0
 def division(n, d):
     return ['{', n, r'\over', d, '}']
 
+
 # [r'{ {{N_a}}', ' \over ', '{{80}} }', ' = ', '{ {{N_d}}', ' \over ', '{{120}} }']
 class CreateHD(Scene):
     
-
 
     def construct(self):
         calcs = [['EV_H = EV_D'],
@@ -61,13 +60,48 @@ class CreateHD(Scene):
         bot_row = mid_row.copy().next_to(mid_row, DOWN, buff=0.0)
         table = VGroup(top_row, mid_row, bot_row).shift(UP)
 
-        cell_test = table[0][1:].get_center()
+        coo = np.empty((3, 3), dtype=object)
+        for i, row in enumerate(table):
+            for j, cell in enumerate(row):
+                  coo[i][j] = cell.get_center()
         
-        text_test = Text('Fight').move_to(cell_test)
+        text_fight_col = Text('Fight').move_to(coo[0, 1])
+        text_fight_row = Text('Fight').move_to(coo[1, 0])
+        
+        text_share_col = Text('Share').move_to(coo[0, 2])
+        text_share_row = Text('Share').move_to(coo[2, 0])
+        
+        payoffs = [1, 1, 3, 0, 0, 3, 2, 2]
+        payoffs_texts = [Text(str(payoff)) for payoff in payoffs]
+        payoffs_anim = payoffs.copy()
+        # f_f_l = Text(str(payoffs[0])).move_to(coo[1,1]).shift(0.7*LEFT)
+        # f_f_r = Text(str(payoffs[1])).move_to(coo[1,1]).shift(0.7*RIGHT)
+        # f_s = Text(payoffs[1]).move_to(coo[1,2])
+        # s_f = Text(payoffs[2]).move_to(coo[2,1])
+        # s_s = Text(payoffs[3]).move_to(coo[2,2])
+        
+        lrud = [LEFT, RIGHT, UP, DOWN]
+        for i, payoff in enumerate(payoffs_texts):
+            payoffs_anim[i] = FadeIn(payoff.move_to(coo[np.floor(i/4+1).astype(int), np.floor((i%4)/2).astype(int)+1]).shift(0.7*lrud[i % 2]))
+        
+        
+        text_below_table = Text('Once again, Prisoner\'s dilemma is \"a reason why we can\'t have nice things.\"', font_size=28).next_to(table.get_bottom(), DOWN).shift(DOWN)
         
         self.play(FadeIn(table))
-        self.play(Create(text_test))
+        self.play(AnimationGroup(Create(text_fight_row), Create(text_share_row)))
+        self.play(AnimationGroup(Create(text_fight_col), Create(text_share_col)))
+        
+        for i in range(4):
+            self.play(AnimationGroup(*payoffs_anim[2*i:2*i+2]))
+        # self.play(FadeIn(f_f_l))
+        # self.play(FadeIn(f_f_r))
+        # self.play(FadeIn(f_s))
+        # self.play(FadeIn(s_f))
+        # self.play(FadeIn(s_s))
+        self.play(Indicate(VGroup(*payoffs_texts[0], *payoffs_texts[2])))
+        self.play(Indicate(VGroup(*payoffs_texts[1], *payoffs_texts[5])))
         self.wait()
+        self.play(Create(text_below_table))
         
         
         
